81b4b86485484fb8fa24d0659fe44cf40e90fcda
faizanshk22 <faizanshk20@gmail.com>
2023-08-08 10:25:38 +0500

    Devise and Social linked added

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "faizanshk@gmail.com.com"
+  layout "mailer"
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+
+
+  
+  root to: 'users#index'
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
