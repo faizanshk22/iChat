682299ddf73cf6852d9efe968ea53bab88bee1f6
faizanshk22 <faizanshk20@gmail.com>
2023-08-10 11:09:31 +0500

    Channel Stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,44 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
     def index
+      @users = current_user
     end
 
     def show
+      @user = User.find(params[:id])  
     end
 
     def new
+      @user = User.new 
     end
 
     def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to root_path, notice: 'User created successfully. Please confirm your email' 
+      else
+        render :new
+      end  
     end
 
     def edit
+      @user = current_user
     end
 
     def update
+      @user = current_user
+       if @user.update(user_params)
+         redirect_to user_path(@user), notice: 'Profile updated successfully.'
+       else
+         render :edit
+       end
     end
 
     def destroy
     end
-
+   
+    private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   
   root to: 'users#index'
   resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
